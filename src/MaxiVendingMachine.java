28a4a8b3982d4e9e788f87500df3ef82a6351c41
Vaztonyo <tony.dewet7@gmail.com>
2019-10-09 12:01:53 +0200

    added new class, MaxiVendingMachine and added overriden buy and add method plus add and buy methods with the different product as a signiture

---
+++ src/MaxiVendingMachine.java
@@ -0,0 +1,54 @@
+import product_exceptions.*;
+import product_exceptions.sub_exceptions.*;
+import products.*;
+
+public class MaxiVendingMachine extends VendingMachine {
+
+    @Override
+    public void buy(Product product) throws ProductNotFoundException {
+            throw new InvalidProductException("Not a valid products");
+    }
+
+    public void buy(Chocolate chocolate) throws ChocolatesAllGone {
+        if (chocolateCounter == 0){
+         throw new ChocolatesAllGone("No Chocolates in Vending Machine");
+        } else {
+            chocolateCounter--;
+        }
+    }
+    public void buy(SoftDrink softdrink) throws SoftDrinksOutOfStockException {
+        if (softDrinkCounter == 0){
+            throw new SoftDrinksOutOfStockException("No more Soft Drinks in Vending Machine");
+        } else {
+            softDrinkCounter--;
+        }
+    }
+    public void buy(SaltySnack saltySnack) throws SaltyCracksAllEatenException {
+        if (saltySnackCounter == 0){
+            throw new SaltyCracksAllEatenException("No Salty Snacks left in Vending Machine");
+        } else {
+            saltySnackCounter--;
+        }
+    }
+
+    @Override
+    public void add(Product product, int add) {
+        throw new InvalidProductException("Not a valid product");
+    }
+
+    public void addStock(SoftDrink softdrink, int add){
+        softDrinkCounter += add;
+    }
+
+    public void addStock(SaltySnack saltySnack, int add){
+        saltySnackCounter += add;
+    }
+
+    public void addStock(Chocolate chocolate, int add){
+        chocolateCounter += add;
+    }
+
+    public static void main(String[] args) {
+        VendingMachine maxiMachine = new MaxiVendingMachine();
+    }
+}
--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,13 +1,11 @@
 import product_exceptions.*;
-import product_exceptions.sub_exceptions.ChocolatesAllGone;
-import product_exceptions.sub_exceptions.SaltyCracksAllEatenException;
-import product_exceptions.sub_exceptions.SoftDrinksOutOfStockException;
+import product_exceptions.sub_exceptions.*;
 import products.*;
 
 public class VendingMachine {
-    private int saltySnackCounter = 0;
-    private int softDrinkCounter = 0;
-    private int chocolateCounter = 0;
+    protected int saltySnackCounter = 0;
+    protected int softDrinkCounter = 0;
+    protected int chocolateCounter = 0;
 
     public void add(Product product, int add) {
         boolean condition = product instanceof SaltySnack || product instanceof Chocolate || product instanceof SoftDrink;
@@ -48,42 +46,21 @@ public class VendingMachine {
         } else if (product instanceof  Chocolate && chocolateCounter == 0){
             throw new ChocolatesAllGone("No Chocolates in Vending Machine");
         }
-
     }
-//    public void buy(Product product, int amount) throws ProductNotFoundException {
-//        if (product instanceof  Chocolate && chocolateCounter != 0) {
-//            chocolateCounter -= amount;
-//        } else {
-//            throw new ChocolatesAllGone("No Chocolates in Vending Machine");
+
+//    public static void main(String[] args) {
+//        VendingMachine machine1 = new VendingMachine();
+//        machine1.add(new Chocolate("Cadbury"), 5);
+//        machine1.add(new SoftDrink("Fanta"), 5);
+//        machine1.add(new SaltySnack("Safari"), 5);
+//        System.out.println( machine1.getStock());
+//        try {
+//            machine1.buy(new SaltySnack("Safari"));
+//            machine1.buy(new SaltySnack("Safari"));
+//        } catch (ProductNotFoundException e) {
+//            e.printStackTrace();
 //        }
 //
-//        if (product instanceof SaltySnack && saltySnackCounter != 0){
-//            saltySnackCounter -= amount;
-//        } else {
-//            throw new SaltyCracksAllEatenException("No more Salty Snacks");
-//        }
-//
-//        if (product instanceof  SoftDrink && softDrinkCounter != 0) {
-//            softDrinkCounter -= amount;
-//        } else {
-//            throw new SoftDrinksOutOfStockException("No Soft Drinks left in the Vending Machine");
-//        }
+//        System.out.println( machine1.getStock());
 //    }
-    
-
-    public static void main(String[] args) {
-        VendingMachine machine1 = new VendingMachine();
-        machine1.add(new Chocolate("Cadbury"), 5);
-        machine1.add(new SoftDrink("Fanta"), 5);
-        machine1.add(new SaltySnack("Safari"), 5);
-        System.out.println( machine1.getStock());
-        try {
-            machine1.buy(new SaltySnack("Safari"));
-            machine1.buy(new SaltySnack("Safari"));
-        } catch (ProductNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println( machine1.getStock());
-    }
 }
